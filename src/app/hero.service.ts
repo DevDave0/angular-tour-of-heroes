0c5e1adf0521350f822b27aecec55927d4414ad0
devdave0 <dbchung24@gmail.com>
2020-12-03 19:16:11 -0500

    simulate a data server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,8 @@ import { HEROES }  from './mock-heroes';
 
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 
 @Injectable({
@@ -11,7 +13,11 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  constructor(  
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]>{
     // TODO: send the message after fetching the heroes
@@ -24,4 +30,10 @@ export class HeroService {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`)
+  }
+
+
 }
